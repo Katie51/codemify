cca1673a30f8b1c07974cf6bc64f4e065abd75e5
Kate <you@example.com>
2024-11-12 13:12:09 -0500

    week5-1

---
+++ week-5/5.1/helper.user.methods.js
@@ -0,0 +1,28 @@
+class Helpers {
+    getUsersByStatus(response, status) {
+
+        const users = response.users.filter((user) => user.role.status == status && user.role.description !== null && user.role.description !== '').map((user) => user.name);
+
+        return users;
+    }
+
+    getUsersByRole(response, role, roleId) {
+        const users = response.users.filter((user) => user.role.name == role && user.role.id == roleId ).map((user) => user.name);
+
+        return users;
+    }
+
+    getUsersByEmailDomain(response, domain) {
+        const users = response.users.filter((user) => user.email.includes(domain)).map((user) => user.name);
+
+        return users;
+    }
+
+    getUserBalanceByCurrency(response, currency) {
+        const balances = response.users.filter((user) => user.currency == currency).map((user) => user.balance);
+
+        return balances;
+    }
+}
+
+export default new Helpers()
