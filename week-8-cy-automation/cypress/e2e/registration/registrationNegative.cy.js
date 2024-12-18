20e66d5af2c6f36ee442543953c23ee6b800ccf3
Katie51 <140750836+Katie51@users.noreply.github.com>
2024-12-17 23:04:05 -0500

    Update registrationNegative.cy.js

--- week-8-cy-automation/cypress/e2e/registration/registrationNegative.cy.js
+++ week-8-cy-automation/cypress/e2e/registration/registrationNegative.cy.js
@@ -1,7 +1,5 @@
 import homePage from "../../page_object/home.page"
 import registrationPage from "../../page_object/registration.page"
-// import homePage from "../../page_object/home.page"
-// import registrationPage from "../../page_object/registration.page"
 
 describe
     ('Registration', () => {
@@ -10,40 +8,25 @@ describe
         })
 
         it('Should not register without email and password', () => {
-            // Click login button on homepage
+          
             homePage.registrBtn.click();
-            //Leave email and password blank
-            // Click on First name
             registrationPage.firstNameInput.type('Kate');
-            // Click on Last name
             registrationPage.lastNameInput.type('Sera');
-            // Leave email and password fields blank
             registrationPage.emailInput.clear();
             registrationPage.passwordInput.clear();
-            //Click register button
             registrationPage.submitBtn.click();
-
-            // Verify user cannot be logged in
             registrationPage.errorEmailReguired.should("be.visible");
             registrationPage.errorPasswordRequired.should("be.visible");
 
         })
 
         it('Should not register with an already existing email account', () => {
-            // Click register button on homepage
             homePage.registrBtn.click();
-
-            // Click on First name
             registrationPage.firstNameInput.type('Kate');
-            // Click on Last name
             registrationPage.lastNameInput.type('Sera');
-            //Tape name which already exists 
             registrationPage.emailInput.type('aro@gmail.com');
             registrationPage.passwordInput.type('aroaro')
-            //Click register button
             registrationPage.submitBtn.click();
-
-            // Verify user cannot be logged in
             registrationPage.errorInputDataValidationFailed.should("be.visible");
 
         })
